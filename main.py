88d5fdbe3ae8a500af8f802beeb316a232985a80
Colton Nishida <coltonkai11@berkeley.edu>
2019-06-11 16:25:11 -0700

    Added File Saves

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import mido
 from mido import Message
-from mido import MidiFile
+from mido import MidiFile, MidiTrack
 
 bach1 = MidiFile('bach_846.mid')
+
+file = MidiFile()
+result = MidiTrack()
+file.tracks.append(result)
 
 for i, track in enumerate(bach1.tracks):
     print('Track {}: {}'.format(i, track.name))
     for msg in track:
         print(msg)
+        result.append(msg)
 
+file.save("Testing.mid")
