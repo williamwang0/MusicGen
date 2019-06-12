cb7397470d34bd2ed32447b996f40287d82b1472
Colton Nishida <coltonkai11@berkeley.edu>
2019-06-12 09:10:17 -0700

    Added Markov Chain and Save Functions

--- main.py
+++ main.py
@@ -1,19 +1,69 @@
 import mido
 from mido import Message
 from mido import MidiFile, MidiTrack
+import numpy as np
 
 bach1 = MidiFile('bach_846.mid')
 
 file = MidiFile()
 
-for i, track in enumerate(bach1.tracks):
-    print('Track {}: {}'.format(i, track.name))
+##############################
+#    Creates Markov Chain    #
+##############################
+
+def createChain(song, channel): #Takes in both a song and a channel to base the chain off of
+    tMatrix = np.zeros((100,100))
+    normalizationVec = np.zeros(100)
+
+
+    prev = 0
+    for i, track in enumerate(bach1.tracks):
+        for msg in track:
+            if msg.type == 'note_on' and msg.channel == channel:
+
+                curr = msg.note
+                if prev != 0:
+                    normalizationVec[prev-1] = normalizationVec[prev-1] + 1
+                    tMatrix[prev-1][curr-1] = tMatrix[prev-1][curr-1] + 1
+                prev = curr
+
+
+    for index in range(100):
+        tMatrix[index] = tMatrix[index] / normalizationVec[i]
+
+    return tMatrix
+
+
+
+
+##############################
+#       Save Midi File       #
+##############################
+
+def saveMidi(sequence, vel, time, name):
+    file = MidiFile()
     result = MidiTrack()
 
-    for msg in track:
-        print(msg)
-        result.append(msg)
+    for num in sequence:
+        result.append(mido.Message('note_on', note=num, velocity=vel, time=time))
 
     file.tracks.append(result)
+    file.save(name)
 
-file.save("Testing.mid")
+
+##############################
+#          Running           #
+##############################
+
+
+chain = createChain(bach1, 2)
+for i in chain:
+    print(i)
+
+
+
+
+
+"""for i, track in enumerate(bach1.tracks):
+    print('Track {}: {}'.format(i, track.name))
+    file.tracks.append(result)"""
